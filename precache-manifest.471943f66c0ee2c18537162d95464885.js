491ed62ad6b8c9aecb08ea4e0096c3e4b4378176
Leo <st831209@gmail.com>
2020-08-21 11:56:32 +0800

    Updates

---
+++ precache-manifest.471943f66c0ee2c18537162d95464885.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720fd578d43a48d1ec1b9964cc1dab30",
+    "url": "/reactFois/index.html"
+  },
+  {
+    "revision": "6e8c7724d314d8ac6ccd",
+    "url": "/reactFois/static/css/2.a3ea2c75.chunk.css"
+  },
+  {
+    "revision": "b929ea68587aef091f5e",
+    "url": "/reactFois/static/css/main.2f426172.chunk.css"
+  },
+  {
+    "revision": "6e8c7724d314d8ac6ccd",
+    "url": "/reactFois/static/js/2.f1b03d89.chunk.js"
+  },
+  {
+    "revision": "e540f6842fff5b618557b20ee9acf94c",
+    "url": "/reactFois/static/js/2.f1b03d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b929ea68587aef091f5e",
+    "url": "/reactFois/static/js/main.05029592.chunk.js"
+  },
+  {
+    "revision": "f6be7627723d6f16115e",
+    "url": "/reactFois/static/js/runtime-main.c9c6c723.js"
+  },
+  {
+    "revision": "b4976a5c91ad8bfb922caacbab0fbee0",
+    "url": "/reactFois/static/media/ChampLogo.b4976a5c.svg"
+  },
+  {
+    "revision": "2751b7e45d0b53eda122430cd2a40aa2",
+    "url": "/reactFois/static/media/FOIS_Web-登入.2751b7e4.jpg"
+  },
+  {
+    "revision": "ac85ee87081d7181e30a477441bc05e6",
+    "url": "/reactFois/static/media/FOIS_Web_home01-compressed.ac85ee87.jpg"
+  },
+  {
+    "revision": "27d1febcac4352b77234188a5cc0da75",
+    "url": "/reactFois/static/media/FOIS_Web_home02-compressed.27d1febc.jpg"
+  },
+  {
+    "revision": "c2f93d4a1ae43ab8fca9fb6cfa5cfa26",
+    "url": "/reactFois/static/media/FOIS_Web_home03-compressed.c2f93d4a.jpg"
+  },
+  {
+    "revision": "057558ab44d79d121e4f1505ef235683",
+    "url": "/reactFois/static/media/WorkInterestBackground.057558ab.jpg"
+  },
+  {
+    "revision": "412aae00986318317a8a900383373b48",
+    "url": "/reactFois/static/media/loginPageBackground-press.412aae00.jpg"
+  },
+  {
+    "revision": "131b690d91ee3ad73a9d495c4433c27f",
+    "url": "/reactFois/static/media/noimage.131b690d.svg"
+  },
+  {
+    "revision": "277f3493d67d85c54d46e7a40ef3678f",
+    "url": "/reactFois/static/media/pin3.277f3493.png"
+  }
+]);
